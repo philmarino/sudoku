40620abf5810d6ff80c115aa20347cf454f25afd
MarinoLizzy <134414988+MarinoLizzy@users.noreply.github.com>
2026-06-14 18:40:03 -0400

    add utils

---
+++ utils.py
@@ -0,0 +1,15 @@
+def print_board(board):
+    '''
+    Output the board as text
+    '''
+    return None
+
+def read_image(image):
+    '''
+    Take a square PNG and return a board object
+    '''
+    return None
+
+if __name__ == "__main__":
+    sample_board = ''
+    print_board(sample_board)
